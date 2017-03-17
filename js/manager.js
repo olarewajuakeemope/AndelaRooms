6641865ff3ca150ff0cd488f27e8ad5dec6fe9f3
olarewajuakeemope <olarewajuakeemopeyemi@gmail.com>
2017-03-17 14:39:22 +0100

    first commit with necessary js files added

---
+++ js/manager.js
@@ -0,0 +1,14 @@
+function userAuthentication(form) {
+  var theUsername = form.username.value;
+  var thePassword = form.password.value;
+
+  $.getJSON("js/user.json", function(user) {
+    if (theUsername === user.username && thePassword === user.password) {
+      
+      window.open("manager.html");
+    } else {
+      window.open("user.html");
+    }
+  })
+}
+
